e7dcecd6842c1874e9f770555aec75da45dd9ba2
Jonathan Forbes <elektrifi@gmail.com>
2012-08-05 19:26:35 +0100

    Initial upload

---
+++ Kf2/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+
+
+namespace Kalman_Filter
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        float px, py, cx, cy, ix = 100, iy;
+        #endregion
+
+        #region Plot Variables
+        double[] data_array_X = new double[100];
+        double[] data_array_Y = new double[100];
+        double[] Kalman_array_Y = new double[100];
+        double[] Kalman_array_X = new double[100];
+        Random rand = new Random();
+        int sine_pos = 0;
+        #endregion
+
+        #region Kalman Filter and Poins Lists
+        PointF[] oup = new PointF[2];
+        private Kalman kal;
+        private SyntheticData syntheticData;
+        private List<PointF> mousePoints;
+        private List<PointF> kalmanPoints;
+        #endregion
+
+        #region Timers
+        Timer SineWave_Timer = new Timer();
+        Timer RandomWave_Timer = new Timer();
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitialiseTimers();
+            InitialiseTimers(100);
+
+            //set up x data for initial condition and scrolling
+            data_array_X[0] = 100;
+            for (int i = 1; i < data_array_X.Length; i++)
+            {
+                data_array_X[i] = i;
+                Kalman_array_X[i] = i;
+                //data_array_Y[i] = rand.Next(100);
+            }
+            SetupChart();
+        }
+        //private void InitialiseTimers(int Timer_Interval = 100)
+        private void InitialiseTimers(int Timer_Interval)
+        {
+            SineWave_Timer.Interval = Timer_Interval;
+            SineWave_Timer.Tick += new EventHandler(SineWave_Timer_Tick);
+            RandomWave_Timer.Interval = Timer_Interval;
+            RandomWave_Timer.Tick += new EventHandler(RandomWave_Timer_Tick);
+        }
+        public void SetupChart()
+        {
+            //Data 
+            //Type and colour
+
+            chart1.Series[0].Label = "The Data";
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            chart1.Series[0].Color = Color.Blue;
+
+            //Axis
+            chart1.ChartAreas[0].AxisX.Title = "X axis";
+            chart1.ChartAreas[0].AxisY.Title = "Y axis";
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            //chart1.ChartAreas[0].AxisX.Maximum = data_array_X.Length - 1;
+
+            //Kalman
+            //Type and colour
+            chart1.Series[1].Label = "Kalman Data";
+            chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            chart1.Series[1].Color = Color.Red;
+
+            Update();
+        }
+        
+       
+        //Setup Kalman Filter and predict methods
+        public void KalmanFilter()
+            {
+                mousePoints = new List<PointF>();
+                kalmanPoints = new List<PointF>();
+                kal = new Kalman(4, 2, 0);
+                syntheticData = new SyntheticData();
+                Matrix<float> state = new Matrix<float>(new float[]
+                {
+                    0.0f, 0.0f, 0.0f, 0.0f
+                });
+                kal.CorrectedState = state;
+                kal.TransitionMatrix = syntheticData.transitionMatrix;
+                kal.MeasurementNoiseCovariance = syntheticData.measurementNoise;
+                kal.ProcessNoiseCovariance = syntheticData.processNoise;
+                kal.ErrorCovariancePost = syntheticData.errorCovariancePost;
+                kal.MeasurementMatrix = syntheticData.measurementMatrix;
+            }
+        public PointF[] filterPoints(PointF pt)
+        {
+            syntheticData.state[0, 0] = pt.X;
+            syntheticData.state[1, 0] = pt.Y;
+            Matrix<float> prediction = kal.Predict();
+            PointF predictPoint = new PointF(prediction[0, 0], prediction[1, 0]);
+            PointF measurePoint = new PointF(syntheticData.GetMeasurement()[0, 0],
+                syntheticData.GetMeasurement()[1, 0]);
+            Matrix<float> estimated = kal.Correct(syntheticData.GetMeasurement());
+            PointF estimatedPoint = new PointF(estimated[0, 0], estimated[1, 0]);
+            syntheticData.GoToNextState();
+            PointF[] results = new PointF[2];
+            results[0] = predictPoint;
+            results[1] = estimatedPoint;
+            px = predictPoint.X;
+            py = predictPoint.Y;
+            cx = estimatedPoint.X;
+            cy = estimatedPoint.Y;
+            return results;
+        }
+
+        //Initialse Wave Function
+        private void Start_BTN_Click(object sender, EventArgs e)
+        {
+            if (Start_BTN.Text == "Start Sine Wave")
+            {
+                KalmanFilter(); //initialize kalman filter
+                SineWave_Timer.Start();
+                Start_BTN.Text = "Stop Sine Wave";
+            }
+            else
+            {
+                SineWave_Timer.Stop();
+                Start_BTN.Text = "Start Sine Wave";
+            }
+        }
+        private void Start_BTN2_Click(object sender, EventArgs e)
+        {
+            if (Start_BTN2.Text == "Start Random")
+            {
+                KalmanFilter(); //initialize kalman filter
+                RandomWave_Timer.Start();
+                Start_BTN2.Text = "Stop Random";
+                timmer_UPDN.Enabled = false;
+            }
+            else
+            {
+                RandomWave_Timer.Stop();
+                Start_BTN2.Text = "Start Random";
+                timmer_UPDN.Enabled = true;
+            }
+        }
+
+        public void SineWave_Timer_Tick(object sender, EventArgs e)
+        {
+            ix++;
+            iy = nextSineWavePosition(ix++);
+
+            //set data
+            Array.Copy(data_array_Y, 1, data_array_Y, 0, 99);
+            data_array_Y[data_array_Y.Length - 1] = iy;
+            Array.Copy(data_array_X, 1, data_array_X, 0, 99);
+            data_array_X[data_array_X.Length - 1] = ix;
+
+            //display data
+            CalculatedData_LBL.Text = "Calculated Data- X:" + ix.ToString() + " Y:" + iy.ToString();
+
+            //updatae kalman and predict
+            PointF inp = new PointF(ix, iy);
+            oup = new PointF[2];
+            oup = filterPoints(inp);
+
+            PointF[] pts = oup;
+
+            KalmanCorrection_LBL.Text = "Kalman Correction- X:" + ((int)cx).ToString() + " Y:" + ((int)cy).ToString();
+            CalculatedPrediction_LBL.Text = "Calculated Prediction- X:" + ((int)px).ToString() + " Y:" + ((int)py).ToString();
+
+            //Set kalman Data
+            Array.Copy(Kalman_array_Y, 1, Kalman_array_Y, 0, 99);
+            Kalman_array_Y[Kalman_array_Y.Length - 1] = cy;
+            Array.Copy(Kalman_array_X, 1, Kalman_array_X, 0, 99);
+            Kalman_array_X[Kalman_array_X.Length - 1] = cx;
+
+            Update();
+            this.Refresh();
+        }
+        public void RandomWave_Timer_Tick(object sender, EventArgs e)
+        {
+            ix++;
+            iy = rand.Next(100);
+
+            //set data
+            Array.Copy(data_array_Y, 1, data_array_Y, 0, 99);
+            data_array_Y[data_array_Y.Length - 1] = iy;
+            Array.Copy(data_array_X, 1, data_array_X, 0, 99);
+            data_array_X[data_array_X.Length - 1] = ix;
+
+            //display data
+            CalculatedData_LBL.Text = "Calculated Data- X:" + ix.ToString() + " Y:" + iy.ToString();
+
+            //updatae kalman and predict
+            PointF inp = new PointF(ix, iy);
+            oup = new PointF[2];
+            oup = filterPoints(inp);
+
+            PointF[] pts = oup;
+
+            KalmanCorrection_LBL.Text = "Kalman Correction- X:" + ((int)cx).ToString() + " Y:" + ((int)cy).ToString();
+            CalculatedPrediction_LBL.Text = "Calculated Prediction- X:" + ((int)px).ToString() + " Y:" + ((int)py).ToString();
+
+            //Set kalman Data
+            Array.Copy(Kalman_array_Y, 1, Kalman_array_Y, 0, 99);
+            Kalman_array_Y[Kalman_array_Y.Length - 1] = cy;
+            Array.Copy(Kalman_array_X, 1, Kalman_array_X, 0, 99);
+            Kalman_array_X[Kalman_array_X.Length - 1] = cx;
+
+            Update();
+            this.Refresh();
+        }
+
+        double amplitude = 100;
+        double frequency = 500;
+
+        public float nextSineWavePosition(float i)
+        {
+            return (float)(amplitude * Math.Sin(2 * Math.PI * frequency * i * 0.00004));
+        }
+        public void Update()
+        {
+            chart1.ChartAreas[0].AxisX.Minimum = data_array_X[0];
+            chart1.Series[0].Points.DataBindXY(data_array_X, data_array_Y);
+            chart1.Series[1].Points.DataBindXY(Kalman_array_X, Kalman_array_Y);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            InitialiseTimers((int)timmer_UPDN.Value);
+        }
+
+
+    }
+}
---
+++ Kf2/Form1.designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Kalman_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Start_BTN = new System.Windows.Forms.Button();
+            this.CalculatedData_LBL = new System.Windows.Forms.Label();
+            this.CalculatedPrediction_LBL = new System.Windows.Forms.Label();
+            this.KalmanCorrection_LBL = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Start_BTN2 = new System.Windows.Forms.Button();
+            this.timmer_UPDN = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timmer_UPDN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start_BTN
+            // 
+            this.Start_BTN.Location = new System.Drawing.Point(12, 9);
+            this.Start_BTN.Name = "Start_BTN";
+            this.Start_BTN.Size = new System.Drawing.Size(97, 30);
+            this.Start_BTN.TabIndex = 2;
+            this.Start_BTN.Text = "Start Sine Wave";
+            this.Start_BTN.UseVisualStyleBackColor = true;
+            this.Start_BTN.Click += new System.EventHandler(this.Start_BTN_Click);
+            // 
+            // CalculatedData_LBL
+            // 
+            this.CalculatedData_LBL.AutoSize = true;
+            this.CalculatedData_LBL.Location = new System.Drawing.Point(241, 9);
+            this.CalculatedData_LBL.Name = "CalculatedData_LBL";
+            this.CalculatedData_LBL.Size = new System.Drawing.Size(86, 13);
+            this.CalculatedData_LBL.TabIndex = 3;
+            this.CalculatedData_LBL.Text = "Calculated Data:";
+            // 
+            // CalculatedPrediction_LBL
+            // 
+            this.CalculatedPrediction_LBL.AutoSize = true;
+            this.CalculatedPrediction_LBL.Location = new System.Drawing.Point(241, 41);
+            this.CalculatedPrediction_LBL.Name = "CalculatedPrediction_LBL";
+            this.CalculatedPrediction_LBL.Size = new System.Drawing.Size(110, 13);
+            this.CalculatedPrediction_LBL.TabIndex = 4;
+            this.CalculatedPrediction_LBL.Text = "Calculated Prediction:";
+            // 
+            // KalmanCorrection_LBL
+            // 
+            this.KalmanCorrection_LBL.AutoSize = true;
+            this.KalmanCorrection_LBL.Location = new System.Drawing.Point(444, 9);
+            this.KalmanCorrection_LBL.Name = "KalmanCorrection_LBL";
+            this.KalmanCorrection_LBL.Size = new System.Drawing.Size(96, 13);
+            this.KalmanCorrection_LBL.TabIndex = 5;
+            this.KalmanCorrection_LBL.Text = "Kalman Correction:";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 67);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "The Data";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Kalman Prediction";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(701, 393);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Start_BTN2
+            // 
+            this.Start_BTN2.Location = new System.Drawing.Point(115, 9);
+            this.Start_BTN2.Name = "Start_BTN2";
+            this.Start_BTN2.Size = new System.Drawing.Size(97, 30);
+            this.Start_BTN2.TabIndex = 7;
+            this.Start_BTN2.Text = "Start Random";
+            this.Start_BTN2.UseVisualStyleBackColor = true;
+            this.Start_BTN2.Click += new System.EventHandler(this.Start_BTN2_Click);
+            // 
+            // timmer_UPDN
+            // 
+            this.timmer_UPDN.Location = new System.Drawing.Point(115, 41);
+            this.timmer_UPDN.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.timmer_UPDN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.timmer_UPDN.Name = "timmer_UPDN";
+            this.timmer_UPDN.Size = new System.Drawing.Size(97, 20);
+            this.timmer_UPDN.TabIndex = 8;
+            this.timmer_UPDN.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.timmer_UPDN.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Interval";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 472);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timmer_UPDN);
+            this.Controls.Add(this.Start_BTN2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.KalmanCorrection_LBL);
+            this.Controls.Add(this.CalculatedPrediction_LBL);
+            this.Controls.Add(this.CalculatedData_LBL);
+            this.Controls.Add(this.Start_BTN);
+            this.Name = "Form1";
+            this.Text = "Cosneta KF2";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timmer_UPDN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_BTN;
+        private System.Windows.Forms.Label CalculatedData_LBL;
+        private System.Windows.Forms.Label CalculatedPrediction_LBL;
+        private System.Windows.Forms.Label KalmanCorrection_LBL;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Start_BTN2;
+        private System.Windows.Forms.NumericUpDown timmer_UPDN;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
